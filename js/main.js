1a9f56de5bead69738b4354de096a022e16a0443
jyjeong <jyjoung2000@gmail.com>
2023-03-02 10:34:36 +0900

    #add - swiper slide

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
+//검색버튼 클릭이벤트
 searchEl.addEventListener('click', function() {
     //Logic
     searchInputEl.focus();
@@ -15,6 +16,7 @@ searchInputEl.addEventListener('blur', function(){
     searchInputEl.setAttribute('placeholder','');
 });
 
+//스크롤에 따른 배지 보이기 & 숨기기
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function() {
@@ -37,10 +39,19 @@ window.addEventListener('scroll', _.throttle(function() {
 // _.throttle(함수, 시간)
 
 
+// 이미지 gsap 애니메이션
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
     gsap.to(fadeEl, 1, {
         delay: (index+1) * .7, //0.7, 1.4, 2.1
         opacity: 1
     });
+});
+
+// SWIPER 사용
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
 });
