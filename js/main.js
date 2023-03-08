75ba45180b142a6aa2ad7693c95769f39e3d2b74
jyjeong <jyjoung2000@gmail.com>
2023-03-08 16:25:42 +0900

    #add - find store

--- js/main.js
+++ js/main.js
@@ -89,3 +89,39 @@ promotionToggleBtn.addEventListener('click',function() {
     }
 })
 
+//범위 랜덤 함수(소숫점 2자리까지)
+function random(min, max) {
+    // 'toFixed()'를 통해 반환된 문자 데이터를,
+    // 'parseFloat()'을 통해 소숫점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max-min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+    // gsap.to(요소, 시간, 옵션);
+    gsap.to(
+        selector, 
+        random(1.5, 2.5), //애니메이션 동작 시간
+        { //옵션
+            y: size,
+            repeat: -1,
+            yoyo: true,
+            ease: Power1.easeInOut,
+            delay: random(0,delay)
+        }
+    );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl) {
+   new ScrollMagic
+       .Scene({
+           triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+           triggerHook: .8
+       })
+       .setClassToggle(spyEl, 'show')
+       .addTo(new ScrollMagic.Controller());
+});
